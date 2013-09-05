ca7baa79b93f7d22d74242927ec266c6db7ad166
OnlyMyRailgun <asceyang@me.com>
2013-09-05 12:45:26 +0800

    finish user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:edit, :update, :index]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
-  
+  before_action :redirect_to_root_path, only: [:new, :create]
   def new
     @user = User.new
   end
@@ -71,5 +71,9 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def redirect_to_root_path
+      redirect_to(root_path) if signed_in?
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -57,7 +57,31 @@ describe "AuthenticationPages" do
         }
         
         describe "after signing in" do
-          specify { expect(page).to have_title("Edit user") }
+          
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+          
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              visit signin_path
+              fill_in "Email", with: user.email
+              fill_in "Password", with: user.password
+              click_button "Sign in"
+            end
+            
+            it "should render the default page" do
+              expect(page).to have_title(user.name)
+            end
+        end
+  
+        describe "after signing out" do
+          before {
+            click_link "Sign out"
+          }
+          it { should_not have_link("Profile") }
+          it { should_not have_link("Settings") } 
         end
       end
 
